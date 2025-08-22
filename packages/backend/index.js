3332daa0e43adcf2638e706a24a7617737de53ec
Oluwaseyi Salami <osalami09@gmail.com>
2025-08-22 04:02:13 +0300

    chore: initialize backend server with Express and middleware setup

---
+++ packages/backend/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (_req, res) => {
+  res.send("Hello from the backend!");
+});
+
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
