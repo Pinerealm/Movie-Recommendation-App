f8c2bcc072bb38233378b2d01d58cfd37f0d15c9
Oluwaseyi Salami <osalami09@gmail.com>
2025-08-22 04:00:02 +0300

    chore: initialize backend configuration with mongoose connection

---
+++ packages/backend/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined");
+    }
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      // useUnifiedTopology: true,
+      // useNewUrlParser: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
